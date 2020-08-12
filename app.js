359f0103ef0796c44d285218c04c96dbc0e1b748
Carlos de la Calleja <carlosdelacalleja@gmail.com>
2020-08-12 19:31:41 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//primitive
+
+
+// String
+
+const name ='john doue';
+
+// Number
+
+const age = 45;
+
+
+const haskids = true;
+
+
+let test;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof haskids);
+
+const today = new Date();
+
+console.log(today);
+
+console.log(typeof today);
